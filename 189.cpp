145c7f6202827eed3cc45ca891dae0ecfac4eb5c
muskanjangi <163982489+muskanjangi@users.noreply.github.com>
2025-09-14 20:27:56 +0530

    Added ques-189 of leetcode

---
+++ 189.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> // for std::reverse
+
+class Solution {
+public:
+    void rotate(std::vector<int>& nums, int k) {
+        int n = nums.size();
+        k %= n;
+        std::reverse(nums.begin(), nums.end());
+        std::reverse(nums.begin(), nums.begin() + k);
+        std::reverse(nums.begin() + k, nums.end());
+    }
+};
+
+int main() {
+    std::vector<int> nums = {1, 2, 3, 4, 5, 6, 7};
+    int k = 3;
+
+    Solution sol;
+    sol.rotate(nums, k);
+
+    std::cout << "Rotated array: ";
+    for (int num : nums) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
